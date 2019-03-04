b288db6e4a9a914da7049d506ce6e7a64cd6e76d
Abdul Khadi <khadi.a@pogeyan.com>
2019-03-04 23:14:29 +0530

    Intial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import { Modal as ReactOverlayModal } from 'react-overlays'
+import { Modal, Button } from 'react-bootstrap'
+const datajs = require('./data/datajs.json');
+
+// var Modal = require('react-bootstrap-modal');
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listvalue: [],
+      showitem: false
+    }
+  }
+  getoption(name) {
+    var listvalue
+    if (name = "offer") {
+      datajs.property.map(e1 => {
+        if (e1.id === name) {
+          listvalue = e1.choice
+        }
+      })
+    }
+    this.setState({
+      listvalue: listvalue,
+      showitem: !this.state.showitem
+    })
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ul>
+          {
+            datajs.property.map(e => <li className="click left" onClick={() => { this.getoption(e.id) }}>
+              {e.id}
+            </li>
+            )}
+        </ul>
+        {this.state.showitem === true ?
+          <div >
+            <Modal.Dialog className="left-size" >
+              <Modal.Body>
+                {
+                  this.state.listvalue.map((item, index) => (
+                    <li key={index}>
+                      {item}
+                    </li>
+                  ))
+                }
+              </Modal.Body>
+            </Modal.Dialog>
+          </div>
+          : ""}
       </div>
     );
   }
